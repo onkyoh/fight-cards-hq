dc1c63cdffaaced6740348b2e34cf239a44664a7
Adnan Radwan <adnanradwan_8@hotmail.com>
2023-09-05 02:28:34 -0400

    defered gtag

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
+// vite.config.js
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import Radar from 'vite-plugin-radar';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		Radar({
+			analytics: {
+				gtm: 'G-6Q7GSW5NYN'
+			},
+			defer: true
+		})
+	]
 });
