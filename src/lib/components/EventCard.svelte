7518e8196dd5ef18da238e4e7e2bcf58da8c24b4
Adnan Radwan <adnanradwan_8@hotmail.com>
2023-09-16 22:24:57 -0400

    fixed timer

--- src/lib/components/EventCard.svelte
+++ src/lib/components/EventCard.svelte
@@ -1,105 +1,123 @@
 <script lang="ts">
-    import { onDestroy, onMount } from "svelte";
-  
-    import ModeToggle from "./ModeToggle.svelte";
-    import Matchup from "./Matchup.svelte";
-  
-    export let event: IEvent;
-    export let toggleEvents: () => void;
-  
-    let timeLeft: number;
-  
-    function dateToMs(dateStr: string) {
-      const dateArray = dateStr.split(" ");
-  
-      for (let i = 0; i < dateArray.length; i++) {
-        if (dateArray[i] === "") {
-          dateArray.splice(i, 1);
-        }
-      }
-  
-      const [weekday, monthStr, dayStr, timeStr, ofDay, tzStr] = dateArray;
-  
-      const month = new Date(Date.parse(`${monthStr} 1, 2000`)).getMonth();
-  
-      let [hours, minutes] = timeStr
-        .split(":")
-        .map((str: string) => parseInt(str));
-  
-      const day = parseInt(dayStr.replace(",", ""));
-  
-      if (ofDay === "PM" && hours !== 12) {
-      hours += 12;
-    }
+	import { onDestroy, onMount } from 'svelte';
 
-    // Get timezone offset from user's device
+	import ModeToggle from './ModeToggle.svelte';
+	import Matchup from './Matchup.svelte';
 
-    const timezoneOffset = new Date().getTimezoneOffset() / 60;
-    hours -= timezoneOffset; // Adjust for timezone
-  
-      const utcDate = new Date(Date.UTC(2023, month, day, hours, minutes));
-  
-      return utcDate.getTime();
-    }
-  
-    const tick = () => {
-      timeLeft -= 1000;
-    };
-  
-    const timer = setInterval(tick, 1000);
-  
-    function formatTimer(duration: number) {
-    if (!duration) return "TBD";
-    let remaining = {
-        seconds: Math.floor((duration / 1000) % 60),
-        minutes: Math.floor((duration / (1000 * 60)) % 60),
-        hours: Math.floor((duration / (1000 * 60 * 60)) % 24),
-        days: Math.floor(duration / (1000 * 60 * 60 * 24)),
-    };
+	export let event: IEvent;
+	export let toggleEvents: () => void;
 
-    for (let time in remaining) {
-        if (remaining.hasOwnProperty(time)) {
-            if (remaining[time as keyof typeof remaining] < 0) {
-                remaining[time as keyof typeof remaining] = 0;
-            }
-        }
-    }
+	let timeLeft: number;
 
-  return `${remaining.days}:${remaining.hours}:${remaining.minutes}:${remaining.seconds}`;
-}
+	function dateToMs(dateStr: string) {
+		const dateArray = dateStr.split(' ').filter((idx) => idx !== '');
 
-  
-    $: timeLeft = dateToMs(event.date) - new Date().getTime();
-  
-    onMount(() => {
-      timeLeft = dateToMs(event.date) - new Date().getTime();
-    });
-    3;
-  
-    onDestroy(() => {
-      clearInterval(timer);
-    });
-  </script>
-  
-  <header>
-    <ModeToggle />
-    <h3>{event.title}</h3>
-    <button id="menu" on:click={toggleEvents}>🥊</button>
-  </header>
-  
-  <h4>{event.date}</h4>
-  <h4 id="timer">{formatTimer(timeLeft)}</h4>
-  <h5>MAIN</h5>
-  <ul>
-    {#each event.fights.filter((fight) => fight.main) as fight}
-      <Matchup matchup={fight} />
-    {/each}
-  </ul>
-  {#if event.fights.filter((fight) => !fight.main).length > 0}
-    <h5>PRELIMS</h5>
-    <ul>
-      {#each event.fights.filter((fight) => !fight.main) as fight}
-        <Matchup matchup={fight} />
-      {/each}
-    </ul>
-  {/if}
+		let eventDate = {
+			year: 0,
+			month: 0,
+			date: 0,
+			hour: 0,
+			minute: 0
+		};
+
+		const months = [
+			'January',
+			'February',
+			'March',
+			'April',
+			'May',
+			'June',
+			'July',
+			'August',
+			'September',
+			'October',
+			'November',
+			'December'
+		];
+
+		//month
+		eventDate.month = months.indexOf(dateArray[1]);
+
+		//year
+		new Date().getMonth() <= eventDate.month
+			? (eventDate.year = new Date().getFullYear())
+			: (eventDate.year = new Date().getFullYear() + 1);
+
+		//date
+		eventDate.date = parseInt(dateArray[2].slice(0, -1));
+
+		let hour: number | string[] = dateArray[3].split(':');
+		//minute
+		eventDate.minute = parseInt(hour[1]);
+
+		//hour
+		hour = parseInt(hour[0]);
+
+		dateArray[4] === 'PM' || (hour === 12 && dateArray[4] === 'AM')
+			? (eventDate.hour = hour + 12)
+			: (eventDate.hour = hour);
+
+		return new Date(
+			eventDate.year,
+			eventDate.month,
+			eventDate.date,
+			eventDate.hour,
+			eventDate.minute
+		).getTime();
+	}
+
+	const tick = () => {
+		timeLeft -= 1000;
+	};
+
+	const timer = setInterval(tick, 1000);
+
+	function formatTimer(duration: number) {
+		if (!duration) return 'TBD';
+		let remaining = {
+			seconds: Math.floor((duration / 1000) % 60),
+			minutes: Math.floor((duration / (1000 * 60)) % 60),
+			hours: Math.floor((duration / (1000 * 60 * 60)) % 24),
+			days: Math.floor(duration / (1000 * 60 * 60 * 24))
+		};
+
+		for (let time in remaining) {
+			if (remaining.hasOwnProperty(time)) {
+				if (remaining[time as keyof typeof remaining] < 0) {
+					remaining[time as keyof typeof remaining] = 0;
+				}
+			}
+		}
+
+		return `${remaining.days}:${remaining.hours}:${remaining.minutes}:${remaining.seconds}`;
+	}
+
+	$: timeLeft = dateToMs(event.date) - new Date().getTime();
+
+	onDestroy(() => {
+		clearInterval(timer);
+	});
+</script>
+
+<header>
+	<ModeToggle />
+	<h3>{event.title}</h3>
+	<button id="menu" on:click={toggleEvents}>🥊</button>
+</header>
+
+<h4>{event.date}</h4>
+<h4 id="timer">{formatTimer(timeLeft)}</h4>
+<h5>MAIN</h5>
+<ul>
+	{#each event.fights.filter((fight) => fight.main) as fight}
+		<Matchup matchup={fight} />
+	{/each}
+</ul>
+{#if event.fights.filter((fight) => !fight.main).length > 0}
+	<h5>PRELIMS</h5>
+	<ul>
+		{#each event.fights.filter((fight) => !fight.main) as fight}
+			<Matchup matchup={fight} />
+		{/each}
+	</ul>
+{/if}
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,15 +1,15 @@
-import {error} from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageLoad} */
 export async function load() {
+	const res = await fetch('https://mmafightcardsapi.adaptable.app/');
 
-    const res = await fetch("https://mmafightcardsapi.adaptable.app/")
+	if (!res.ok)
+		throw error(500, {
+			message: 'Error, refresh page'
+		});
 
-    if (!res.ok) throw error(500, {
-        message: 'Error, refresh page'
-    });
+	const doc = await res.json();
 
-    const doc = await res.json()
-
-    return { doc }
+	return { doc };
 }
