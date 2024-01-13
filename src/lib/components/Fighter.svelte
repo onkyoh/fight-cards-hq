59cadd979cd450908ae406ad57bea35f0cfacc30
onkyoh <adnanradwan_8@hotmail.com>
2024-01-12 21:12:04 -0500

    fallback img fighter

--- src/lib/components/Fighter.svelte
+++ src/lib/components/Fighter.svelte
@@ -5,12 +5,15 @@
 		const trimmedRecord = record.trim();
 		return trimmedRecord && !isNaN(parseInt(trimmedRecord[0])) ? trimmedRecord : 'N/A';
 	};
+
+	const displayImage = (img: string) => {
+		return fighter.picture.includes('https') ? img : '/src/assets/fallbackFighter.gif';
+	};
 </script>
 
 <a class="fighter" href={fighter.link}>
-	<img src={fighter.picture} alt={fighter.name} />
+	<img src={displayImage(fighter.picture)} alt={fighter.name} />
 	<div>
-		{fighter.name}
 		<div class="country-record">
 			<img src={fighter.country} alt={fighter.name + ' country of origin'} />{displayRecord(
 				fighter.record
